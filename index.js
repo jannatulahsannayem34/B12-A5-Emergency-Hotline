52a6c49d05e470629de4743066a1038b76cf25db
jannatulahsannayem34 <jannatulahsannayem34@gmail.com>
2025-10-30 19:48:59 +0600

    fourt

--- index.js
+++ index.js
@@ -11,25 +11,52 @@ function getClickResult(id) {
 }
 
 // for call
-document.getElementById('call-1').addEventListener('click',function(){
-   
-    const header = document.getElementById('header').innerText;
-    const number = document.getElementById('num').innerText;
-    alert('📞 calling '+ header +' '+  number + '... ');
-    
+function toCall(id) {
+    document.getElementById(id).addEventListener('click', function () {
+        const header = document.getElementById('header').innerText;
+        const number = document.getElementById('num').innerText;
+        alert('📞 calling ' + header + ' ' + number + '... ');
+        const coinElement = document.getElementById('coin');
+        const coinInt = parseInt(coinElement.innerText);
+        if (coinInt < 20) {
+            alert('❌ insufficient coin!!!')
+            return;
+        }
+        
+        const availableCoin = coinInt - 20;
+        coinElement.innerText = availableCoin;
+    })
 
-})
+}
 
 
 
+// for 1
 getClickResult('heart-1')
-getClickResult('heart-2')
-getClickResult('heart-3')
-getClickResult('heart-4')
-getClickResult('heart-5')
-getClickResult('heart-6')
-getClickResult('heart-7')
-getClickResult('heart-8')
-getClickResult('heart-9')
+toCall('call-1')
 
+
+getClickResult('heart-2')
+toCall('call-2')
+
+getClickResult('heart-3')
+toCall('call-3')
+
+getClickResult('heart-4')
+toCall('call-4')
+
+getClickResult('heart-5')
+toCall('call-5')
+
+getClickResult('heart-6')
+toCall('call-6')
+
+getClickResult('heart-7')
+toCall('call-7')
+
+getClickResult('heart-8')
+toCall('call-8')
+
+getClickResult('heart-9')
+toCall('call-9')
 
