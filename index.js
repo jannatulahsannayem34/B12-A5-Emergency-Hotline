f95a60a083f060f26efa88c88f157542458cd3b8
jannatulahsannayem34 <jannatulahsannayem34@gmail.com>
2025-10-31 10:23:58 +0600

    sixth commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // console.log('nayem')
+let copyCount = 0;
 let clickCount = 0;
 let historiesData = []
 // function for icon
@@ -8,6 +9,22 @@ function getClickResult(id) {
         clickCount++;
         const clickResult = document.getElementById('click-result')
         clickResult.innerText = clickCount;
+    })
+}
+
+//function for copy count
+function getCopyCount(id) {
+    document.getElementById(id).addEventListener('click', function (e) {
+        e.preventDefault;
+        const card = e.currentTarget.closest('.bg-white');
+        const number = card.querySelector('.card-num').innerText;
+
+        alert(` Number copied: ${number}`)
+        navigator.clipboard.writeText(number);
+        
+        copyCount++;
+        const clickResult = document.getElementById('copy-result-btn')
+        clickResult.innerText = copyCount;
     })
 }
 
@@ -22,7 +39,7 @@ function toCall(id) {
         const coinElement = document.getElementById('coin');
         const coinInt = parseInt(coinElement.innerText);
         if (coinInt < 20) {
-            alert('❌ insufficient coin!!!')
+            alert('❌ insufficient coin!!!Need 20 coins for further calling')
             return;
         }
         alert('📞 calling ' + header + ' ' + number + '... ');
@@ -38,8 +55,6 @@ function toCall(id) {
 
         }
         historiesData.push(data);
-
-
         renderHistory();
     })
 }
@@ -65,40 +80,47 @@ function renderHistory() {
         historyContainer.appendChild(div);
     }
 }
+document.getElementById('clear-btn').addEventListener('click', function () {
+    document.getElementById('history-container').innerHTML = '';
+    return;
 
-
-
-
-
-// for 1
-
+})
 
 getClickResult('heart-1')
 toCall('call-1')
+getCopyCount('copy-1')
 
 
 getClickResult('heart-2')
 toCall('call-2')
+getCopyCount('copy-2')
 
 getClickResult('heart-3')
 toCall('call-3')
+getCopyCount('copy-3')
 
 getClickResult('heart-4')
 toCall('call-4')
+getCopyCount('copy-4')
 
 getClickResult('heart-5')
 toCall('call-5')
+getCopyCount('copy-5')
 
 getClickResult('heart-6')
 toCall('call-6')
+getCopyCount('copy-6')
 
 getClickResult('heart-7')
 toCall('call-7')
+getCopyCount('copy-7')
 
 getClickResult('heart-8')
 toCall('call-8')
+getCopyCount('copy-8')
 
 getClickResult('heart-9')
 toCall('call-9')
+getCopyCount('copy-9')
 
 
